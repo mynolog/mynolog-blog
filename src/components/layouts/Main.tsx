6e819f5bf308242d5854c5fa00ad1a49c8a009cc
mynolog <minholee.log@gmail.com>
2025-04-03 17:10:30 +0900

    style: Header 컴포넌트 타이틀 중앙 정렬 및 Main 컴포넌트 패딩값 수정

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -5,8 +5,6 @@ interface MainProps {
 
 export default function Main({ children, className = '' }: MainProps) {
   return (
-    <main className={`flex w-full max-w-[1200px] grow px-6 ${className}`}>
-      {children}
-    </main>
+    <main className={`flex w-full max-w-[1100px] grow p-9 ${className}`}>{children}</main>
   )
 }
