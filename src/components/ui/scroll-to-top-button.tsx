7eba21b414b95c2b0c35baa197b31992d3d2ee9f
mynolog <minholee.log@gmail.com>
2025-04-13 15:13:06 +0900

    style: markdown 및 selection 스타일 수정

--- src/components/ui/scroll-to-top-button.tsx
+++ src/components/ui/scroll-to-top-button.tsx
@@ -25,7 +25,7 @@ export default function ScrollToTopButton() {
     showButton && (
       <Button
         onClick={scrollToTop}
-        className="fixed right-8 bottom-16 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-black/80 p-3 text-xl text-white shadow-2xl transition-all hover:cursor-pointer hover:bg-gray-900 hover:ring-2 hover:ring-white"
+        className="fixed right-8 bottom-16 z-50 flex h-12 w-12 items-center justify-center rounded-full bg-black/80 p-3 text-xl text-white shadow-2xl transition-all hover:cursor-pointer hover:bg-gray-900 hover:ring-2 hover:ring-white dark:bg-white dark:text-black"
       >
         <ChevronUp />
       </Button>
