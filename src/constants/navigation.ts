1c15876aa2c66c69d6df19dd862a5636878d5432
mynolog <minholee.log@gmail.com>
2025-05-09 18:41:43 +0900

    chore: 미완성 포트폴리오 페이지 주석 처리

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -6,11 +6,12 @@ export const NAVIGATION_ITEMS = [
     label: 'blog',
     href: ROUTES.BLOG,
   },
-  {
-    id: '2000',
-    label: 'portfolio',
-    href: ROUTES.PORTFOLIO,
-  },
+  // TODO: 포트폴리오 페이지 추가
+  // {
+  //   id: '2000',
+  //   label: 'portfolio',
+  //   href: ROUTES.PORTFOLIO,
+  // },
   {
     id: '3000',
     label: 'about',
